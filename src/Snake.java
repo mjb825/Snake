407ad34514d29f44fd7d4365084b2d10ef504e44
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2018-11-13 13:19:56 -0500

    Getting rid of Head class

--- src/Snake.java
+++ src/Snake.java
@@ -1,4 +1,4 @@
-import java.util.List;
+import java.util.ArrayList;
 /**
  * container class for SnakeBody pieces
  * - keep track of position of each piece
@@ -7,26 +7,47 @@ import java.util.List;
 public class Snake
 {
     private Head head;
-    private List<Tail> tail;
+    private ArrayList<Tail> tail;
     private Direction currentDirection;
     private int previousX;
     private int previousY;
     private int x;
     private int y;
+    private Move currentMove;
+    
+    public Snake(){}
     
     public Snake(int x, int y)
     {
+        tail = new ArrayList<>();
         head = new Head(x, y);
+        currentMove = new Move(x, y, Direction.UP);
+        tail.add(new Tail(currentMove));
     }
     
     public void updateFrame(int rangeX, int rangeY)
     {
+                
+        if(currentDirection == Direction.UP && y > 0) {
+            y--;
+        } else if(currentDirection == Direction.DOWN && y < (rangeY - 1)) {
+            y++;
+        } else if(currentDirection == Direction.LEFT && x > 0) {
+            x--;
+        } else if(currentDirection == Direction.RIGHT && x < (rangeX - 1)){
+            x++;
+        }
+        
+        //list.get(0).updateFrame(0,0)
+        //setCenterX(x * 20 + 10);
+        //setCenterY(y * 20 + 10);
+        
         head.updateFrame(rangeX, rangeY);
     }
     
-    public Head getHead()
+    public Tail getHead()
     {
-        return head;
+        return tail.get(0);
     }
     
     public void setDirection(Direction direction)
