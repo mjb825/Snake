ad89cdf2605862044399db753413ebdc3d4cc029
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2018-11-14 11:04:28 -0500

    It's actually looking like a game!!!

--- src/GameField.java
+++ src/GameField.java
@@ -1,32 +1,34 @@
-    
-    
-    import javafx.application.Application;
-    import javafx.event.ActionEvent;
-    import javafx.event.EventHandler;
-    import javafx.scene.Scene;
-    import javafx.geometry.Insets;
-    import javafx.scene.control.Button;
-    import javafx.scene.control.Label;
-    import javafx.scene.layout.GridPane;
-    import javafx.stage.Stage;
-    
-    import javafx.scene.layout.Pane;
-    
-    import javafx.animation.Timeline;
-    import javafx.animation.KeyFrame;
-    import javafx.util.Duration;
-    import javafx.animation.Animation;
-    import javafx.scene.input.KeyCode;
-    import javafx.scene.input.KeyEvent;
-    import java.util.ArrayList;
-    
-    public class GameField extends Pane
-    {    
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import javafx.scene.layout.Pane;
+
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+import javafx.util.Duration;
+import javafx.animation.Animation;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import java.util.ArrayList;
+
+public class GameField extends Pane
+{    
     private Snake player; // will be class Snake in future
     private Timeline frameTimer;
-    
+    private Food food;
+
     public GameField()
     {
+        // start player in center of field
         player = new Snake(20, 15);
         /*
         player.add(20, 16);
@@ -48,29 +50,31 @@
         player.add(20, 32);
         player.add(20, 33);
 */
-        
-        
-        
+
+
+
         for(int i = 0; i < player.getTail().size(); i++)
             getChildren().add(player.getTail().get(i));
-        
-        
-        //addPiece();
-        
-        
-        //getChildren().add(player.add());
 
+
+        //addPiece();
+
+
+        //getChildren().add(player.add());
         
-        
+        food = new Food();
+        generateFood();
+        getChildren().add(food);
+
         frameTimer = new Timeline(new KeyFrame(Duration.seconds(1.0/8.0),
             e->updateFrame()));
-            
+
         frameTimer.setCycleCount(Animation.INDEFINITE);
     }
-    
+
     public void addPiece()
     {
-        
+
         ArrayList<Tail> tail = player.getTail();
         Tail lastPiece = tail.get(tail.size() - 1).copy();
         Move move = lastPiece.getCurrentMove();
@@ -90,11 +94,11 @@
         else if(direction == Direction.RIGHT)
             player.add(move.getX() - 1, move.getY(), direction);
             //move.setX(move.getX() - 0);
-        
-        
+
+
         getChildren().add(tail.get(tail.size() - 1));
-        
-        
+
+
         /*
         player.add();
         ArrayList<Tail> tail = player.getTail();
@@ -102,18 +106,57 @@
         getChildren().add(lastPiece);
         */
     }
-    
+
+    public void generateFood()
+    {
+        boolean found = true;
+        // coordinates for food (upper limit = 39, 29) (n * 20 + 10) (800x600)
+        int x = 40;
+        int y = 30;
+        
+        // food generation restrictions based off of snake pieces coordinates
+        ArrayList<Tail> tail = player.getTail();
+
+        while(found)
+        {
+            x = (int)(Math.random() * 40);
+            y = (int)(Math.random() * 30);
+            
+            for (Tail piece : tail) {
+                if(x == piece.getCurrentMove().getX()) {
+                    found = true;
+                    break;
+                } else if(y == piece.getCurrentMove().getY()) {
+                    found = true;
+                    break;
+                } else {
+                    found = false;
+                }
+            }
+        }
+        
+        food.setX(x);
+        food.setY(y);
+        
+        food.setCenterX(x * 20 + 10);
+        food.setCenterY(y * 20 + 10);
+
+
+    }
+
     public void updateFrame()
     {
         Tail head = player.getTail().get(0);
         Move move = head.getCurrentMove();
         Direction direction = move.getDirection();
-        if(move.getX() == 19 && move.getY() == 15)
+        if(move.getX() == food.getX() && move.getY() == food.getY()) {
             addPiece();
+            generateFood();
+        }
         player.updateFrame(40, 30);
         //System.out.println("x: " + player.getX() + " y: " + player.getY());
     }
-    
+
     public void handleKey(KeyEvent ke)
     {
         if(ke.getCode() == KeyCode.UP)
@@ -127,7 +170,7 @@
         else if(ke.getCode() == KeyCode.SPACE)
             addPiece();
     }
-    
+
     public void play()
     {
         frameTimer.play();
